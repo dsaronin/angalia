47a1ec0c1db18031641e4f51af697881772c6fdd
daudi amani <dsaronin@gmail.com>
2025-10-12 16:15:29 -0700

    v2.2 - display patient name

--- app/version.rb
+++ app/version.rb
@@ -4,7 +4,7 @@ module Angalia              # Define the top-level module  Angalia::
 # +++++++++++++++++++++++++++++++++++++++++++++++++
 
 module Angalia
-  VERSION = "2.1"
+  VERSION = "2.2"
 end
 
 end  # module Angalia
